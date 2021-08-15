e369c96313a9b4dbc556ce724c338add7ca614c1
fbryo21 <febryryo21@gmail.com>
2021-08-15 09:57:02 +0700

    ganti router jadi voucher

--- server/router/voucher-router/index.js
+++ server/router/voucher-router/index.js
@@ -1,11 +1,11 @@
-const router = require("express").Router();
+const voucher = require("express").Router();
 const controller = require("../../controllers/voucherController");
 const authentication = require("../../middleware/authentication");
 
-router.post("/voucher", authentication, controller.tambahVoucher);
-router.get("/voucher", authentication, controller.getAllVoucher);
-router.get("/voucher/:id", authentication, controller.getOneVoucher);
-router.put("/voucher/:id", authentication, controller.editVoucher);
-router.delete("/voucher/:id", authentication, controller.deleteVoucher);
+voucher.post("/voucher", authentication, controller.tambahVoucher);
+voucher.get("/voucher", authentication, controller.getAllVoucher);
+voucher.get("/voucher/:id", authentication, controller.getOneVoucher);
+voucher.put("/voucher/:id", authentication, controller.editVoucher);
+voucher.delete("/voucher/:id", authentication, controller.deleteVoucher);
 
-module.exports = alamat;
+module.exports = voucher;
