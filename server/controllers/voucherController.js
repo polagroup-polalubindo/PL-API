80d450f697295dedd1584496fc750fd7902644e9
prasetiodc <mprasetiodc@gmail.com>
2021-08-25 15:50:40 +0700

    add pagination in voucher

--- server/controllers/voucherController.js
+++ server/controllers/voucherController.js
@@ -1,4 +1,5 @@
 const { Voucher, Produk, VoucherProduct } = require("../models");
+const Op = require('sequelize').Op
 
 class Controller {
   static tambahVoucher = async (req, res) => {
@@ -106,19 +107,85 @@ class Controller {
   };
 
   static deleteVoucher = async (req, res) => {
-    await Voucher.destroy({ where: { id: req.params.id } });
-    await VoucherProduct.destroy({ where: { voucerId: req.params.id } });
+    try {
+      await Voucher.destroy({ where: { id: req.params.id } });
+      await VoucherProduct.destroy({ where: { voucherId: req.params.id } });
 
-    return res
-      .status(201)
-      .json({ status: "success", id_deleted: req.params.id });
+      return res
+        .status(200)
+        .json({ status: "success", id_deleted: req.params.id });
+    } catch (err) {
+      return res.status(500).json({ err })
+    }
   };
 
   static getAllVoucher = async (req, res) => {
-    const data = await Voucher.findAll({
-      include: { model: VoucherProduct, include: Produk },
-    });
-    return res.status(201).json({ status: "success", data });
+    try {
+      let {
+        page,
+        limit,
+        status,
+        keyword
+      } = req.query
+
+      let allVoucher, totalVoucher, query = {}, condition = {}
+      if (limit) {
+        let offset = +page, condition = {}, query = {}
+        if (offset > 0) offset = offset * +limit
+        query = { offset, limit: +limit }
+      }
+
+      if (status) {
+        if (status === "Sedang Berjalan") {
+          condition = {
+            [Op.and]: [
+              {
+                periodeStart: {
+                  [Op.lte]: new Date()
+                }
+              }, {
+                periodeEnd: {
+                  [Op.gte]: new Date()
+                }
+              }
+            ]
+          }
+        } else if (status === "Akan Datang") {
+          condition = {
+            periodeStart: {
+              [Op.gte]: new Date()
+            }
+          }
+        } else if (status === "Telah Berakhir") {
+          condition = {
+            periodeEnd: {
+              [Op.lte]: new Date()
+            }
+          }
+        }
+      }
+
+      if (keyword) {
+        condition = {
+          ...condition,
+          [Op.or]: [
+            { name: { [Op.substring]: keyword } },
+            { code: { [Op.substring]: keyword } },
+          ]
+        }
+      }
+      // condition = { statusVoucher: status }
+
+      allVoucher = await Voucher.findAll({ where: condition, include: { model: VoucherProduct, include: Produk }, ...query, order: [['id', 'DESC']] });
+      let getAllVoucher = await Voucher.findAll({ where: condition });
+      totalVoucher = getAllVoucher.length
+
+      return res.status(201).json({ status: "success", data: allVoucher, totalVoucher });
+
+    } catch (err) {
+      console.log(err)
+      res.status(500).json(err)
+    }
   };
 
   static getOneVoucher = async (req, res) => {
