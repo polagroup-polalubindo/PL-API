1b56a4dea7adfb65e7a38c8959c0e54d2702dafd
prasetiodc <mprasetiodc@gmail.com>
2021-10-19 16:20:53 +0700

    testing

--- server/controllers/machineController.js
+++ server/controllers/machineController.js
@@ -93,7 +93,6 @@ class Controller {
             '$Warranty.hasClaim$': 0,
             '$Warranty.hasClaim$': null,
             '$Warranty.isValid$': 1,
-            'Warranty': null,
           }
         } else {
           condition = {
